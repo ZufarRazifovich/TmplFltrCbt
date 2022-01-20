b70d7fb28593dbc6df8f41f8fbe11d3c8fdca504
mkzlkz <90604924+mkzlkz@users.noreply.github.com>
2022-01-20 08:45:05 +0600

    secondCommit

--- lib/presentation/screens/third_screen.dart
+++ lib/presentation/screens/third_screen.dart
@@ -42,7 +42,7 @@ class _ThirdScreenState extends State<ThirdScreen> {
                 return Column(
                   children: [
                     Text(
-                      'kol vo'
+                      'kol voo'
                     ),
                     Text(
                     state.counterValue.toString(),
